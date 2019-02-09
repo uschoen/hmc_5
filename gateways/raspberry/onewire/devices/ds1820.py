1c284353690c267829e7df38f927529db34a878c
uschoen <uschoen@johjoh.de>
2019-02-09 22:33:10 +0100

    rebuild default devices

---
+++ gateways/raspberry/onewire/devices/ds1820.py
@@ -0,0 +1,25 @@
+'''
+Created on 09.02.2019
+@author: ullrich schoen
+
+'''
+
+# Standard library imports
+import logging
+# Local application imports
+from gateways.hmc.devices.masterDevice import masterDevice
+
+__version__="5.1"
+__author__="ullrich schoen"
+__DEVICENTYPE__="ds1820"
+__DEVICEPACKAGE__="raspberry.onewire"
+
+LOG=logging.getLogger(__name__)
+
+class deviceManager(masterDevice):
+    def __init__(self,deviceID,core,deviceCFG={},restore=False):
+        deviceConfig=deviceCFG
+        deviceConfig['devicePackage']=__DEVICEPACKAGE__
+        deviceConfig['deviceType']=__DEVICENTYPE__
+        masterDevice.__init__(self, deviceID, core, deviceConfig,restore)
+        LOG.info("init deviceID:%s type:%s version:%s"%(self.deviceID,__DEVICENTYPE__,__version__))
